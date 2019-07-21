a52a1bd98174ac6598567b812b19f65de9088c36
Mike Shaunnessey <mikshau@gmail.com>
2019-07-21 10:07:29 -0400

    Update analyzeFace.js

--- analyzeFace.js
+++ analyzeFace.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 const azure = require('azure-storage');
 var qresp ;
 var COMP_FACE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceid=true&returnFaceLandmarks=true&returnFaceAttributes=age,gender,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise' ;
-var imagePath = "https://sdcinststorage.blob.core.windows.net"  ;
+var COMP_FACE_PATH = 'detect?returnFaceid=true&returnFaceLandmarks=true&returnFaceAttributes=age,gender,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise' ;
+var STOR_IMAGE_URL = "https://sdcinststorage.blob.core.windows.net"  ;
 
 module.exports =  function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
     context.log("Making axios POST");
- //   context.log(imagePath) ;
- var bod =  imagePath + myQueueItem ;
- var surl = { 'url' :  bod  } ;
+    var bod =  STOR_IMAGE_URL + myQueueItem ;
+    var surl = { 'url' :  bod  } ;
     context.log("surl" + surl) ;
 
     axios.post(COMP_FACE_URL , surl , {
